6458814b9734354394d04b0dc5fd1df68d7c0e63
Hugo <h@hugo.sx>
2020-04-18 15:13:15 +1000

    Fix small syntax error

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const handler = require('./handler');
 const Worker = require('./worker');
 
-const envVars = [
+let envVars = [
   'LND_IP',
   'LND_PORT',
   'MACAROON_BASE64',
