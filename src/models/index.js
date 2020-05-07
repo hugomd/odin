18de8f17a0416b0f0e2aa22a7ea73e977b1f1b45
Hugo <h@hugo.sx>
2020-05-07 21:11:10 +1000

    Revert sequelize model index.js

--- src/models/index.js
+++ src/models/index.js
@@ -5,10 +5,15 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const {dbConnectionUrl} = require('../config')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-const sequelize = new Sequelize(dbConnectionUrl);
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
